b32cec1db15bf499a4d5ff789b80adfd041b3d2e
Elaine <omwengaelaine@gmail.com>
2025-06-26 00:45:57 +0300

    create protectroute wrapper

---
+++ styleflex-frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+function ProtectedRoute({ children }) {
+const { auth } = useContext(AuthContext);
+if (!auth.isAuthenticated) {
+return <Navigate to="/login" replace />;
+}
+return children;
+}
+export default ProtectedRoute;
