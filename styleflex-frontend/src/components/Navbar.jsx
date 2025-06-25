0cd96cc0449bc862fd8543d0aa3a028791c013bf
Stacey jepchumba <Staceyjepchumbarr@gmail.com>
2025-06-26 00:04:30 +0300

    done

--- styleflex-frontend/src/components/Navbar.jsx
+++ styleflex-frontend/src/components/Navbar.jsx
@@ -2,19 +2,7 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function Navbar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem("access_token");
-    setIsLoggedIn(!!token);
-  }, []);
-
-  const handleLogout = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("refresh_token");
-    window.location.href = "/login";
-  };
-
+  con
   return (
     
             </button>
