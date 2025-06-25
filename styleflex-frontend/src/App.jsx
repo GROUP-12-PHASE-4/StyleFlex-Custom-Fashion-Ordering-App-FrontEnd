9493622dad0292a5da8f121475b85940996e167b
Elaine <omwengaelaine@gmail.com>
2025-06-26 00:53:45 +0300

    update app

--- styleflex-frontend/src/App.jsx
+++ styleflex-frontend/src/App.jsx
@@ -6,20 +6,25 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Orders from "./pages/Orders";
 import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-    <div className="px-4 py-6">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>  
+      <Navbar />
+      <div className="px-4 py-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/orders" element={
+            <ProtectedRoute>
+              <Orders />
+            </ProtectedRoute>
+          } />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>  
     </BrowserRouter>
   );
 }
