7f07986a08de5f30e2143f079f4bd36686fdf1cc
Stacey jepchumba <Staceyjepchumbarr@gmail.com>
2025-06-26 00:33:39 +0300

    done

--- styleflex-frontend/src/App.jsx
+++ styleflex-frontend/src/App.jsx
@@ -1,16 +1,25 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Orders from "./pages/Orders";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
+    <div className="px-4 py-6">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
+    </div>  
     </BrowserRouter>
   );
 }
