bb68c3442febf588b385d97f5174afb2cb9f152b
Stacey jepchumba <Staceyjepchumbarr@gmail.com>
2025-06-25 16:00:36 +0300

    first commit

---
+++ styleflex-frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+Navbar.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="/login">Login</Link> |{" "}
+      <Link to="/signup">Signup</Link> |{" "}
+      <Link to="/dashboard">Dashboard</Link>
+    </nav>
+  );
+}
+
+export default Navbar; 
+
