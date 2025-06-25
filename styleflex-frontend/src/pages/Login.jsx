49eebd088b80c5bfd1a0307cf9c725beecacf433
Stacey jepchumba <Staceyjepchumbarr@gmail.com>
2025-06-26 01:02:29 +0300

    done

--- styleflex-frontend/src/pages/Login.jsx
+++ styleflex-frontend/src/pages/Login.jsx
@@ -2,16 +2,4 @@ import { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-function Login() {
-  const { setAuth } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-
-  const [error, setError] = useState(null);
-
-  
-    
+f
