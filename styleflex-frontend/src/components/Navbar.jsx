88883453a1fbdbf8257429b3efdf7f3619c21701
Stacey jepchumba <Staceyjepchumbarr@gmail.com>
2025-06-26 00:04:43 +0300

    done

--- styleflex-frontend/src/components/Navbar.jsx
+++ styleflex-frontend/src/components/Navbar.jsx
@@ -1,10 +1,5 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-function Navbar() {
-  con
-  return (
-    
-            </button>
-         
+f
          
