ebde014eb196903ba77b2f7d74f8e3c84a0e2a24
Elaine <omwengaelaine@gmail.com>
2025-06-25 23:41:57 +0300

    connnect backend to frontend

--- styleflex-frontend/src/Services/api.js
+++ styleflex-frontend/src/Services/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 const API = axios.create({
- baseURL: 'https://styleflex-custom-fashion-ordering-app.onrender.com/api',
+baseURL: 'https://styleflex-custom-fashion-ordering-app.onrender.com/api',
+headers: {
+'Content-Type': 'application/json',
+},
 });
 export default API;
